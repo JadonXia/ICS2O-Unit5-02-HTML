00769139b034871461964d0b257eb7134c135c7a
JadonXia <jadon.xia@ocsbstudent.ca>
2021-03-16 15:37:45 +0000

    added new buttons.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-//  This links my HTML button with JavaScript.
-document.getElementById('button1').addEventListener('click', display)
-function display () {
+//  This links my HTML button with JavaScript. An alert will appear when the button is clicked.
+document.getElementById('button1').addEventListener('click', displayalert)
+function displayalert () {
   alert('sike')
 }
+
+//This button will change font, size, and color of a paragraph in HTML. Hopefully.
