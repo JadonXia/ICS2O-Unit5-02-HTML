4718eb8610d91e1a0cec87678c0863a993f7781e
JadonXia <jadon.xia@ocsbstudent.ca>
2021-03-16 16:34:22 +0000

    added comments, spaced '='

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //  This links my HTML button with JavaScript. An alert will appear when the button is clicked.
 document.getElementById('button1').addEventListener('click', displayalert)
+//  This will also link with an HTML button, changing font, color, and size of a certain paragraph.
 document.getElementById('button2').addEventListener('click', changeparagraph)
 
 function displayalert () {
@@ -7,7 +8,7 @@ function displayalert () {
 }
 
 function changeparagraph () {
-  document.getElementById('changeparagraph').style.fontSize='150%'
-  document.getElementById('changeparagraph').style.color='red'
-  document.getElementById('changeparagraph').style.fontFamily='fantasy'
+  document.getElementById('changeparagraph').style.fontSize ='150%'
+  document.getElementById('changeparagraph').style.color ='red'
+  document.getElementById('changeparagraph').style.fontFamily ='fantasy'
 }
