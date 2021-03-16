97d183ed4923777bb6b2f53b9d90328ba285e55a
JadonXia <jadon.xia@ocsbstudent.ca>
2021-03-16 16:30:01 +0000

    moved html button elements to javascript

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 //  This links my HTML button with JavaScript. An alert will appear when the button is clicked.
 document.getElementById('button1').addEventListener('click', displayalert)
+document.getElementById('button2').addEventListener('click', changeparagraph)
+
 function displayalert () {
   alert('sike')
+}
+
+function changeparagraph () {
+  document.getElementById('changeparagraph').style.fontSize='150%'
+  document.getElementById('changeparagraph').style.color='red'
+  document.getElementById('changeparagraph').style.fontFamily='fantasy'
 }
